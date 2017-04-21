1882a0478bd1ece64a8a65345f21a1e27020e29a
Mohammad Kermani <98kermani@gmail.com>
2017-04-21 20:27:27 +0430

    popup menu added for chrome extension
    Signed-off-by: Mohammad Kermani <98kermani@gmail.com>

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,4 +1,5 @@
 ///<reference path="../../node_modules/@types/chrome/index.d.ts" />
 
 //This just calls once the extension installed.
-chrome.tabs.create({"url": "http://google.com"});
+//chrome.tabs.create({"url": "http://google.com"});
+//chrome.browserAction.onClicked.addListener(function(tab) { alert('icon clicked')});
